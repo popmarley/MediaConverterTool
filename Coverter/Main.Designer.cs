8451b7e2bf8af91f20a79a6fa3d5113b5aad85da
popmarley <hsynozguven@hotmail.com>
2023-11-28 11:17:00 +0300

    Guncelleme (Sürükle bırak eventi eklendi)

--- Coverter/Main.Designer.cs
+++ Coverter/Main.Designer.cs
@@ -31,6 +31,7 @@
 			this.btnSelectAndConvert = new System.Windows.Forms.Button();
 			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
 			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+			this.panel1 = new System.Windows.Forms.Panel();
 			this.SuspendLayout();
 			// 
 			// btnSelectAndConvert
@@ -47,11 +48,19 @@
 			// 
 			this.openFileDialog1.FileName = "openFileDialog1";
 			// 
+			// panel1
+			// 
+			this.panel1.Location = new System.Drawing.Point(12, 82);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(331, 171);
+			this.panel1.TabIndex = 1;
+			// 
 			// Main
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.panel1);
 			this.Controls.Add(this.btnSelectAndConvert);
 			this.Name = "Main";
 			this.Text = "Pop Coverter | V1.0";
@@ -64,6 +73,7 @@
 		private System.Windows.Forms.Button btnSelectAndConvert;
 		private System.Windows.Forms.OpenFileDialog openFileDialog1;
 		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+		private System.Windows.Forms.Panel panel1;
 	}
 }
 
--- Coverter/Main.cs
+++ Coverter/Main.cs
@@ -18,27 +18,68 @@ namespace Coverter
 		public Main()
 		{
 			InitializeComponent();
+			this.AllowDrop = true;
+			this.DragEnter += new DragEventHandler(this.Main_DragEnter);
+			this.DragDrop += new DragEventHandler(this.Main_DragDrop);
 		}
 
 		private void btnSelectAndConvert_Click(object sender, EventArgs e)
 		{
-			// Dosya seçimi
-			if (openFileDialog1.ShowDialog() == DialogResult.OK)
+			while (true) // Sonsuz döngüye giriyoruz
 			{
-				string sourcePath = openFileDialog1.FileName;
-
-				// Kaydetme dialogunu aç
-				if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+				// Dosya seçimi
+				if (openFileDialog1.ShowDialog() == DialogResult.OK)
 				{
-					string targetPath = saveFileDialog1.FileName;
-					// Burada dönüşüm işlemlerini gerçekleştirin
-					ConvertFile(sourcePath, targetPath);
+					string sourcePath = openFileDialog1.FileName;
+
+					saveFileDialog1.Filter = "Video Files (*.mp4;*.avi;*.mkv;*.mov;*.wmv;*.flv;*.webm;*.3gp;*.m4v)|*.mp4;*.avi;*.mkv;*.mov;*.wmv;*.flv;*.webm;*.3gp;*.m4v|" +
+								 "Image Files (*.jpg;*.jpeg;*.png;*.gif;*.bmp;*.tiff;*.tif;*.svg;*.eps)|*.jpg;*.jpeg;*.png;*.gif;*.bmp;*.tiff;*.tif;*.svg;*.eps|" +
+								 "Audio Files (*.mp3;*.wav;*.flac;*.aac;*.ogg;*.wma;*.m4a;*.midi;*.mid)|*.mp3;*.wav;*.flac;*.aac;*.ogg;*.wma;*.m4a;*.midi;*.mid|" +
+								 "All Files (*.*)|*.*";
+
+					// Kaydetme diyalogunu aç
+					if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+					{
+						string targetPath = saveFileDialog1.FileName;
+
+						// Desteklenmeyen dönüşüm kontrolü
+						if (IsUnsupportedConversion(Path.GetExtension(sourcePath).ToLower(), Path.GetExtension(targetPath).ToLower()))
+						{
+							MessageBox.Show("Bu tür dönüşüm desteklenmiyor.");
+							continue; // Kullanıcıya tekrar deneme şansı ver
+						}
+
+						// Desteklenen dönüşüm varsa, dönüşüm işlemini başlat ve döngüden çık
+						ConvertFile(sourcePath, targetPath);
+						break;
+					}
+					else
+					{
+						// Kullanıcı 'Kaydet' diyalogunda iptal butonuna basarsa döngüden çık
+						break;
+					}
+				}
+				else
+				{
+					// Kullanıcı 'Aç' diyalogunda iptal butonuna basarsa döngüden çık
+					break;
 				}
 			}
 		}
 
 		private void ConvertFile(string sourcePath, string targetPath)
 		{
+			// Kaynak ve hedef dosya uzantılarını kontrol et
+			string sourceExtension = Path.GetExtension(sourcePath).ToLower();
+			string targetExtension = Path.GetExtension(targetPath).ToLower();
+
+			// Desteklenmeyen dönüşüm kontrolü
+			if (IsUnsupportedConversion(sourceExtension, targetExtension))
+			{
+				MessageBox.Show("Bu tür dönüşüm desteklenmiyor.");
+				return;
+			}
+
 			try
 			{
 				string arguments = BuildFfmpegArguments(sourcePath, targetPath);
@@ -83,6 +124,7 @@ namespace Coverter
 			string extension = Path.GetExtension(sourcePath).ToLower();
 			switch (extension)
 			{
+				//video dosyaları
 				case ".mp4":
 				case ".avi":
 				case ".mkv":
@@ -93,6 +135,8 @@ namespace Coverter
 				case ".3gp":
 				case ".m4v":
 					return $"-i \"{sourcePath}\" \"{targetPath}\""; // Video dönüştürme için özelleştirilmiş komutlar eklenebilir
+
+				//resim dosyaları
 				case ".jpg":
 				case ".jpeg":
 				case ".png":
@@ -103,11 +147,101 @@ namespace Coverter
 				case ".svg":
 				case ".eps":
 					return $"-i \"{sourcePath}\" \"{targetPath}\""; // Resim dönüştürme için özelleştirilmiş komutlar eklenebilir
-																	// Diğer dosya türleri için ek komutlar eklenebilir
+				
+				//ses dosyaları
+				case ".mp3":
+				case ".wav":
+				case ".flac":
+				case ".aac":
+				case ".ogg":
+				case ".wma":
+				case ".m4a":
+				case ".midi":
+				case ".mid":
+					return $"-i \"{sourcePath}\" \"{targetPath}\"";
+
 				default:
 					throw new InvalidOperationException("Desteklenmeyen dosya türü.");
 			}
 		}
+
+		private bool IsUnsupportedConversion(string sourceExt, string targetExt)
+		{
+			// Resim uzantıları
+			var imageExtensions = new HashSet<string> { ".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".tif", ".svg", ".eps" };
+
+			// Video uzantıları
+			var videoExtensions = new HashSet<string> { ".mp4", ".avi", ".mkv", ".mov", ".wmv", ".flv", ".webm", ".3gp", ".m4v" };
+
+			// Ses uzantıları
+			var audioExtensions = new HashSet<string> { ".mp3", ".wav", ".flac", ".aac", ".ogg", ".wma", ".m4a", ".midi", ".mid" };
+
+			// Resimden videoya veya sese dönüşüm kontrolü
+			if (imageExtensions.Contains(sourceExt) && (videoExtensions.Contains(targetExt) || audioExtensions.Contains(targetExt)))
+			{
+				return true;
+			}
+
+			// Burada diğer desteklenmeyen dönüşümler için de kontrol ekleyebilirsiniz
+
+			return false;
+		}
+
+		private void Main_DragEnter(object sender, DragEventArgs e)
+		{
+			if (e.Data.GetDataPresent(DataFormats.FileDrop))
+			{
+				e.Effect = DragDropEffects.Copy;
+			}
+			else
+			{
+				e.Effect = DragDropEffects.None;
+			}
+		}
+
+		private void Main_DragDrop(object sender, DragEventArgs e)
+		{
+			string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+			if (files != null && files.Length != 0)
+			{
+				string sourcePath = files[0]; // İlk dosyanın yolunu al
+				ProcessFile(sourcePath);
+			}
+		}
+
+		private void ProcessFile(string sourcePath)
+		{
+			while (true)
+			{
+				saveFileDialog1.Filter = "Video Files (*.mp4;*.avi;*.mkv;*.mov;*.wmv;*.flv;*.webm;*.3gp;*.m4v)|*.mp4;*.avi;*.mkv;*.mov;*.wmv;*.flv;*.webm;*.3gp;*.m4v|" +
+										 "Image Files (*.jpg;*.jpeg;*.png;*.gif;*.bmp;*.tiff;*.tif;*.svg;*.eps)|*.jpg;*.jpeg;*.png;*.gif;*.bmp;*.tiff;*.tif;*.svg;*.eps|" +
+										 "Audio Files (*.mp3;*.wav;*.flac;*.aac;*.ogg;*.wma;*.m4a;*.midi;*.mid)|*.mp3;*.wav;*.flac;*.aac;*.ogg;*.wma;*.m4a;*.midi;*.mid|" +
+										 "All Files (*.*)|*.*";
+
+				// Kaydetme diyalogunu aç
+				if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+				{
+					string targetPath = saveFileDialog1.FileName;
+
+					// Desteklenmeyen dönüşüm kontrolü
+					if (IsUnsupportedConversion(Path.GetExtension(sourcePath).ToLower(), Path.GetExtension(targetPath).ToLower()))
+					{
+						MessageBox.Show("Bu tür dönüşüm desteklenmiyor.");
+						continue; // Kullanıcıya tekrar deneme şansı ver
+					}
+
+					// Desteklenen dönüşüm varsa, dönüşüm işlemini başlat ve döngüden çık
+					ConvertFile(sourcePath, targetPath);
+					break;
+				}
+				else
+				{
+					// Kullanıcı kaydetme diyalogunda iptal butonuna basarsa döngüden çık
+					break;
+				}
+			}
+		}
+
 	}
 }
 
