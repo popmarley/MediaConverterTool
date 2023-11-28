acedd4f370d6384da9e877b88cb0a41ba6b23f85
popmarley <hsynozguven@hotmail.com>
2023-11-28 10:35:34 +0300

    Güncelleme ( Dönüştürme işlemleri eklendi)

---
+++ Coverter/Main.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Coverter
+{
+	partial class Main
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnSelectAndConvert = new System.Windows.Forms.Button();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+			this.SuspendLayout();
+			// 
+			// btnSelectAndConvert
+			// 
+			this.btnSelectAndConvert.Location = new System.Drawing.Point(352, 303);
+			this.btnSelectAndConvert.Name = "btnSelectAndConvert";
+			this.btnSelectAndConvert.Size = new System.Drawing.Size(75, 23);
+			this.btnSelectAndConvert.TabIndex = 0;
+			this.btnSelectAndConvert.Text = "Seç";
+			this.btnSelectAndConvert.UseVisualStyleBackColor = true;
+			this.btnSelectAndConvert.Click += new System.EventHandler(this.btnSelectAndConvert_Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// Main
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.btnSelectAndConvert);
+			this.Name = "Main";
+			this.Text = "Pop Coverter | V1.0";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnSelectAndConvert;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+	}
+}
+
---
+++ Coverter/Main.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace Coverter
+{
+	public partial class Main : Form
+	{
+		private string ffmpegPath = @"D:\ffmpeg-2023-11-27-git-0ea9e26636-full_build\bin\ffmpeg.exe"; // FFmpeg yolunu buraya girin veya bir ayar/config dosyasından alın
+		public Main()
+		{
+			InitializeComponent();
+		}
+
+		private void btnSelectAndConvert_Click(object sender, EventArgs e)
+		{
+			// Dosya seçimi
+			if (openFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				string sourcePath = openFileDialog1.FileName;
+
+				// Kaydetme dialogunu aç
+				if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+				{
+					string targetPath = saveFileDialog1.FileName;
+					// Burada dönüşüm işlemlerini gerçekleştirin
+					ConvertFile(sourcePath, targetPath);
+				}
+			}
+		}
+
+		private void ConvertFile(string sourcePath, string targetPath)
+		{
+			try
+			{
+				string arguments = BuildFfmpegArguments(sourcePath, targetPath);
+
+				var process = new Process
+				{
+					StartInfo = new ProcessStartInfo
+					{
+						FileName = ffmpegPath,
+						Arguments = arguments,
+						UseShellExecute = false,
+						CreateNoWindow = true,
+						RedirectStandardOutput = true,
+						RedirectStandardError = true
+					}
+				};
+
+				process.Start();
+				string output = process.StandardOutput.ReadToEnd();
+				string error = process.StandardError.ReadToEnd();
+				process.WaitForExit();
+
+				if (process.ExitCode != 0)
+				{
+					MessageBox.Show("Dönüştürme sırasında hata oluştu: " + error);
+				}
+				else
+				{
+					MessageBox.Show("Dönüştürme başarıyla tamamlandı.");
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Bir hata oluştu: " + ex.Message);
+			}
+		}
+
+
+
+		private string BuildFfmpegArguments(string sourcePath, string targetPath)
+		{
+			string extension = Path.GetExtension(sourcePath).ToLower();
+			switch (extension)
+			{
+				case ".mp4":
+				case ".avi":
+				case ".mkv":
+				case ".mov":
+				case ".wmv":
+				case ".flv":
+				case ".webm":
+				case ".3gp":
+				case ".m4v":
+					return $"-i \"{sourcePath}\" \"{targetPath}\""; // Video dönüştürme için özelleştirilmiş komutlar eklenebilir
+				case ".jpg":
+				case ".jpeg":
+				case ".png":
+				case ".gif":
+				case ".bmp":
+				case ".tiff":
+				case ".tif":
+				case ".svg":
+				case ".eps":
+					return $"-i \"{sourcePath}\" \"{targetPath}\""; // Resim dönüştürme için özelleştirilmiş komutlar eklenebilir
+																	// Diğer dosya türleri için ek komutlar eklenebilir
+				default:
+					throw new InvalidOperationException("Desteklenmeyen dosya türü.");
+			}
+		}
+	}
+}
+
+
+
